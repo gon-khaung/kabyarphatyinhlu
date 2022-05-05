89aafcabff0f2e0de7f29cc02f61ee6b9c15ba53
Hein Ko Zin <heinkozin4@gmail.com>
2022-05-05 22:01:23 +0630

    🐞fix: commented music state codes.

--- lib/cubit/music_state.dart
+++ lib/cubit/music_state.dart
@@ -1,21 +1,21 @@
-part of 'music_cubit.dart';
+// part of 'music_cubit.dart';
 
-abstract class MusicState extends Equatable {
-  const MusicState();
+// abstract class MusicState extends Equatable {
+//   const MusicState();
 
-  @override
-  List<Object> get props => [];
-}
+//   @override
+//   List<Object> get props => [];
+// }
 
-class MusicInitial extends MusicState {}
+// class MusicInitial extends MusicState {}
 
-class MusicLoading extends MusicState {}
+// class MusicLoading extends MusicState {}
 
-class LoadedMusic extends MusicState {
-  final List<Music> musics;
+// class LoadedMusic extends MusicState {
+//   final List<Music> musics;
 
-  const LoadedMusic({this.musics = const <Music>[]});
+//   const LoadedMusic({this.musics = const <Music>[]});
 
-  @override
-  List<Object> get props => [musics];
-}
+//   @override
+//   List<Object> get props => [musics];
+// }
